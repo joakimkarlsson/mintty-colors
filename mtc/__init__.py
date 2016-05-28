b7719b5205408992d2e89ccfec69f50cf254385b
jkr <joakim.karlsson@lundalogik.se>
2016-05-28 11:22:25 +0200

    Create function for setting colors

--- mtc/__init__.py
+++ mtc/__init__.py
@@ -4,22 +4,53 @@ TMUX = os.environ.get('TMUX')
 ESC_BEGIN = '\033Ptmux;\033\033]' if TMUX else '\033]'
 ESC_END = '\a\033\\' if TMUX else '\a'
 
-print('{}10;#000000{}'.format(ESC_BEGIN, ESC_END))  # Foreground
-print('{}11;#C0C0C0{}'.format(ESC_BEGIN, ESC_END))  # Background
-print('{}12;#00FF00{}'.format(ESC_BEGIN, ESC_END))  # Cursor
-print('{}4;0;#000000{}'.format(ESC_BEGIN, ESC_END))  # black
-print('{}4;1;#BF0000{}'.format(ESC_BEGIN, ESC_END))  # red
-print('{}4;2;#00BF00{}'.format(ESC_BEGIN, ESC_END))  # green
-print('{}4;3;#BFBF00{}'.format(ESC_BEGIN, ESC_END))  # yellow
-print('{}4;4;#0000BF{}'.format(ESC_BEGIN, ESC_END))  # blue
-print('{}4;5;#BF00BF{}'.format(ESC_BEGIN, ESC_END))  # magenta
-print('{}4;6;#00BFBF{}'.format(ESC_BEGIN, ESC_END))  # cyan
-print('{}4;7;#BFBFBF{}'.format(ESC_BEGIN, ESC_END))  # white
-print('{}4;8;#404040{}'.format(ESC_BEGIN, ESC_END))  # bold black
-print('{}4;9;#FF4040{}'.format(ESC_BEGIN, ESC_END))  # bold red
-print('{}4;10;#40FF40{}'.format(ESC_BEGIN, ESC_END))  # bold green
-print('{}4;11;#FFFF40{}'.format(ESC_BEGIN, ESC_END))  # bold yellow
-print('{}4;12;#6060FF{}'.format(ESC_BEGIN, ESC_END))  # bold blue
-print('{}4;13;#FF40FF{}'.format(ESC_BEGIN, ESC_END))  # bold magenta
-print('{}4;14;#40FFFF{}'.format(ESC_BEGIN, ESC_END))  # bold cyan
-print('{}4;15;#FFFFFF{}'.format(ESC_BEGIN, ESC_END))  # bold white
+COLOR_NAME_CODES = dict(
+    ForegroundColour='10;',
+    BackgroundColour='11;',
+    CursorColour='12;',
+    Black='4;0;',
+    Red='4;1;',
+    Green='4;2;',
+    Yellow='4;3;',
+    Blue='4;4;',
+    Magenta='4;5;',
+    Cyan='4;6;',
+    White='4;7;',
+    BoldBlack='4;8;',
+    BoldRed='4;9;',
+    BoldGreen='4;10;',
+    BoldYellow='4;11;',
+    BoldBlue='4;12;',
+    BoldMagenta='4;13;',
+    BoldCyan='4;14;',
+    BoldWhite='4;15;',
+)
+
+
+def set_color(name, val):
+    print('{begin}{color_name}{val}{end}'.format(
+        begin=ESC_BEGIN,
+        color_name=COLOR_NAME_CODES[name],
+        val=val,
+        end=ESC_END))
+
+
+set_color('ForegroundColour', '#000000')
+set_color('BackgroundColour', '#C0C0C0')
+set_color('CursorColour', '#00FF00')
+set_color('Black', '#000000')
+set_color('Red', '#BF0000')
+set_color('Green', '#00BF00')
+set_color('Yellow', '#BFBF00')
+set_color('Blue', '#0000BF')
+set_color('Magenta', '#BF00BF')
+set_color('Cyan', '#00BFBF')
+set_color('White', '#BFBFBF')
+set_color('BoldBlack', '#404040')
+set_color('BoldRed', '#FF4040')
+set_color('BoldGreen', '#40FF40')
+set_color('BoldYellow', '#FFFF40')
+set_color('BoldBlue', '#6060FF')
+set_color('BoldMagenta', '#FF40FF')
+set_color('BoldCyan', '#40FFFF')
+set_color('BoldWhite', '#FFFFFF')
